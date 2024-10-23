4fdb356c20fa60cdcc65050801505c7744424e99
KimGyungSik <0711kyungh@naver.com>
2024-10-23 16:03:50 +0900

    first commit

---
+++ src/main/java/shoppingmall/ankim/AnkimApplication.java
@@ -0,0 +1,13 @@
+package shoppingmall.ankim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnkimApplication.class, args);
+    }
+
+}
