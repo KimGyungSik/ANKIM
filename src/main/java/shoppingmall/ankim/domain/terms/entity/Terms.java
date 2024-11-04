bd3f8d55a8642f774c373e2b3a7c9f99192ebe22
JUNGHOON AN <gnstm831@naver.com>
2024-11-04 14:59:30 +0900

    refactor: TermsHistoryId 제거
    TermsHistory 엔티티에서 복합키 TermsHistoryId를 제거하고, 자동증가 컬럼을 추가하여 유연성 확보

--- src/main/java/shoppingmall/ankim/domain/terms/entity/Terms.java
+++ src/main/java/shoppingmall/ankim/domain/terms/entity/Terms.java
@@ -12,11 +12,15 @@ import java.util.List;
 public class Terms {
 
     @Id
-    @Column(length = 7)
-    private String code; // 구분-번호
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long no;
 
-    @Column(name = "prents_code", length = 7, nullable = false)
-    private String prentsCode; // 상위 약관 코드
+    @ManyToOne
+    @JoinColumn(name = "prents_no") // 부모 약관의 FK로 참조
+    private Terms parentTerms;
+
+    @Column(name = "code", length = 7)
+    private String code; // 구분-번호
 
     @Column(length = 200, nullable = false)
     private String name; // 약관명
@@ -25,15 +29,12 @@ public class Terms {
     private String contents; // 약관 내용
 
     @Column(name = "terms_yn", length = 1, nullable = false)
-    private String termsYn; // 필수 동의 여부
+    private String termsYn = "N"; // 필수 동의 여부
 
     @Column(nullable = false)
     private Integer level; // 약관 레벨
 
     @Column(name = "active_yn", length = 1, nullable = false)
     private String activeYn = "Y"; // 활성화 상태
-
-    @OneToMany(mappedBy = "terms")
-    private List<TermsHistory> termsHistories; // 연관된 약관 동의 이력
 
 }
--- src/main/java/shoppingmall/ankim/domain/terms/entity/TermsHistory.java
+++ src/main/java/shoppingmall/ankim/domain/terms/entity/TermsHistory.java
@@ -3,6 +3,7 @@ package shoppingmall.ankim.domain.terms.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import shoppingmall.ankim.domain.member.entity.Member;
 
 import java.time.LocalDateTime;
 
@@ -11,18 +12,23 @@ import java.time.LocalDateTime;
 @Table(name = "terms_history")
 public class TermsHistory {
 
-    @EmbeddedId
-    private TermsHistoryId id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "no")
+    private Long no;
 
     @ManyToOne
-    @MapsId("code") // 복합 키 id의 code 필드와 매핑
-    @JoinColumn(name = "code", insertable = false, updatable = false, nullable = false)
-    private Terms terms; // 약관 코드 (FK)
+    @JoinColumn(name = "mem_no", nullable = false)
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "terms_no", nullable = false)
+    private Terms terms;
 
     @Column(name = "terms_yn", length = 1, nullable = false)
-    private String termsYn; // 동의 여부
+    private String termsYn = "N"; // 동의 여부
 
     @Column(name = "agree_date", nullable = false)
-    private LocalDateTime agreeDate; // 동의 일자
+    private LocalDateTime agreeDate = LocalDateTime.now(); // 동의 일자
 
 }
