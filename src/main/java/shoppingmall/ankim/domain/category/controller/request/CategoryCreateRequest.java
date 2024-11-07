8e17320c06c94ca70677f0a754a333558a9dcbb4
KimGyungSik <0711kyungh@naver.com>
2024-11-07 23:14:24 +0900

    Category엔티티에 요구사항에 적합한 Validation 추가

--- src/main/java/shoppingmall/ankim/domain/category/controller/request/CategoryCreateRequest.java
+++ src/main/java/shoppingmall/ankim/domain/category/controller/request/CategoryCreateRequest.java
@@ -1,4 +1,30 @@
 package shoppingmall.ankim.domain.category.controller.request;
 
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shoppingmall.ankim.domain.category.entity.CategoryLevel;
+
+@Getter
+@NoArgsConstructor
 public class CategoryCreateRequest {
+
+    @NotBlank(message = "카테고리 이름은 필수 입력 값입니다.")
+    @Size(max = 50, message = "카테고리 이름은 50자 이하로 입력해야 합니다.")
+    private String name;
+
+    @NotNull(message = "카테고리 레벨은 필수 입력 값입니다.")
+    private CategoryLevel level; // 중분류(MIDDLE) 또는 소분류(SUB) 여부를 나타냅니다.
+
+    // 중분류일 때는 null일 수 있지만, 소분류일 때는 상위 카테고리 ID가 필요합니다.
+    private Long parentNo;
+
+    public CategoryCreateRequest(String name, CategoryLevel level, Long parentNo) {
+        this.name = name;
+        this.level = level;
+        this.parentNo = parentNo;
+    }
 }
--- src/main/java/shoppingmall/ankim/global/exception/ErrorCode.java
+++ src/main/java/shoppingmall/ankim/global/exception/ErrorCode.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "카테고리를 찾을 수 없습니다.");
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "카테고리를 찾을 수 없습니다."),
+    CATEGORY_NAME_TOO_LONG(HttpStatus.BAD_REQUEST, "카테고리 이름은 50자 이하로 입력해야 합니다.");
 
     private final HttpStatus httpStatus;
 
