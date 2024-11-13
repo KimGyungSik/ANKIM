b5d217c18438673d76a9bd2fad89f4772ddcbcf3
KimGyungSik <0711kyungh@naver.com>
2024-11-13 23:48:29 +0900

    ProductService 상품 등록 테스트 완료

--- src/main/java/shoppingmall/ankim/domain/item/repository/ItemRepository.java
+++ src/main/java/shoppingmall/ankim/domain/item/repository/ItemRepository.java
@@ -3,5 +3,8 @@ package shoppingmall.ankim.domain.item.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shoppingmall.ankim.domain.item.entity.Item;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item,Long> {
+    List<Item> findByProduct_No(Long prodNo);
 }
