df6f4189e4a40cd14c37d3c12da798c8a4870925
KimGyungSik <0711kyungh@naver.com>
2024-11-26 23:26:40 +0900

    [feat] MemberAddressRepository에 기본 주소지 가져오는 쿼리 추가

--- src/main/java/shoppingmall/ankim/domain/address/repository/MemberAddressRepository.java
+++ src/main/java/shoppingmall/ankim/domain/address/repository/MemberAddressRepository.java
@@ -1,11 +1,17 @@
 package shoppingmall.ankim.domain.address.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import shoppingmall.ankim.domain.address.entity.member.MemberAddress;
 import shoppingmall.ankim.domain.member.entity.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberAddressRepository extends JpaRepository<MemberAddress, Long> {
     List<MemberAddress> findByMember(Member member);
+
+    @Query("SELECT ma FROM MemberAddress ma WHERE ma.member = :member AND ma.defaultAddressYn = 'Y'")
+    Optional<MemberAddress> findDefaultAddressByMember(@Param("member") Member member);
 }
