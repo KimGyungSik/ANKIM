ced88bf6d516e5c205a40151d0eb49ee04d6cd7d
KimGyungSik <0711kyungh@naver.com>
2024-11-27 17:24:34 +0900

    [feat] application.yml에 TossPayment 환경변수 설정 완료

--- src/main/java/shoppingmall/ankim/domain/payment/entity/Payment.java
+++ src/main/java/shoppingmall/ankim/domain/payment/entity/Payment.java
@@ -3,6 +3,8 @@ package shoppingmall.ankim.domain.payment.entity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import shoppingmall.ankim.domain.order.entity.Order;
+import shoppingmall.ankim.global.audit.BaseEntity;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -10,25 +12,26 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "Payment")
-public class Payment {
+public class Payment extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long no;
 
+    @OneToOne(fetch = FetchType.LAZY)
     @Column(name = "ord_no", nullable = false)
-    private Long ordNo;
+    private Order order;
 
-    private String type;
+    private String type; // 결제 유형
 
     @Column(name = "total_price", precision = 10, scale = 2)
-    private BigDecimal totalPrice;
+    private BigDecimal totalPrice; // 총 결제 금액
 
-    private String name;
+    private String name; // 결제명
 
     @Column(name = "pay_key")
-    private String payKey;
+    private String payKey; // 결제키
 
     @Column(name = "pay_date")
-    private LocalDateTime payDate;
+    private LocalDateTime payDate; // 결제 요청 일시
 }
 
