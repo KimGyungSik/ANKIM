a09d981b1b05baae02684419adf8b346289b2272
KimGyungSik <0711kyungh@naver.com>
2024-12-23 20:46:14 +0900

    [Refactor] PaymentControllerTest 수정

--- src/main/java/shoppingmall/ankim/domain/address/controller/request/MemberAddressCreateRequest.java
+++ src/main/java/shoppingmall/ankim/domain/address/controller/request/MemberAddressCreateRequest.java
@@ -2,12 +2,16 @@ package shoppingmall.ankim.domain.address.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import shoppingmall.ankim.domain.address.service.request.MemberAddressCreateServiceRequest;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MemberAddressCreateRequest {
 
     private String addressName;         // 주소 이름
