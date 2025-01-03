910f6c8519d91b4d3161fe87f518e6a581ae0b73
KimGyungSik <0711kyungh@naver.com>
2025-01-03 19:03:00 +0900

    [Refactor] 결제 시스템 Redis를 이용한 재고 차감,복구 테스트 완료

--- src/main/java/shoppingmall/ankim/global/config/RedisConfig.java
+++ src/main/java/shoppingmall/ankim/global/config/RedisConfig.java
@@ -1,6 +1,9 @@
 package shoppingmall.ankim.global.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +23,17 @@ public class RedisConfig {
 
     @Value("${spring.data.redis.port}")
     private int port;
+
+    // Redis 연결 주소의 접두사로 사용할 상수 선언
+    private static final String REDISSON_HOST_PREFIX = "redis://";
+
+    @Bean
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
+
+        return Redisson.create(config);
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
--- src/main/java/shoppingmall/ankim/global/config/lock/LockHandler.java
+++ src/main/java/shoppingmall/ankim/global/config/lock/LockHandler.java
@@ -2,10 +2,14 @@ package shoppingmall.ankim.global.config.lock;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
 import shoppingmall.ankim.domain.item.repository.ItemLockRepository;
 import shoppingmall.ankim.domain.item.repository.ItemRepository;
 import shoppingmall.ankim.domain.product.repository.ProductRepository;
+
+import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -13,6 +17,7 @@ import shoppingmall.ankim.domain.product.repository.ProductRepository;
 public class LockHandler {
     private static final String LOCK_KEY_PREFIX = "LOCK_";
     private final ItemLockRepository itemLockRepository;
+    private final RedissonClient redissonClient;
 
     public void lock(String key) {
         Long available = itemLockRepository.getLock(LOCK_KEY_PREFIX + key);
@@ -27,5 +32,22 @@ public class LockHandler {
     public void unlock(String key) {
         itemLockRepository.releaseLock(LOCK_KEY_PREFIX + key);
         log.info("Lock released for key: {}", LOCK_KEY_PREFIX + key);
+    }
+
+    public void lockWithRedisson(String key, long waitTime, long leaseTime) {
+        RLock lock = redissonClient.getLock(LOCK_KEY_PREFIX + key);
+        try {
+            boolean available = lock.tryLock(waitTime, leaseTime, TimeUnit.SECONDS);
+            if(!available) {
+                throw new InterruptedException("LOCK_ACQUISITION_FAILED");
+            }
+        }catch (InterruptedException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public void unlockWithRedisson(String key) {
+        RLock lock = redissonClient.getLock(LOCK_KEY_PREFIX + key);
+        lock.unlock();
     }
 }
