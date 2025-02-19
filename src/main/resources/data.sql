cd7e545f61e349ec423a524137aa4843d6ef31d4
KimGyungSik <0711kyungh@naver.com>
2025-02-19 10:32:49 +0900

    [style] 장바구니 담기 API 연결

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -152,27 +152,27 @@ INSERT INTO item_option (item_no, optv_no) VALUES
 -- VALUES (null, 'ORDER', '주문 약관', 'ANKIM 주문/결제 약관', 'Y', 'v1', 1, 'Y');
 --
 -- -- [ 회원 삽입 ]
--- INSERT INTO member (
---     login_id,
---     pwd,
---     name,
---     phone_num,
---     birth,
---     gender,
---     grade,
---     join_date,
---     status
--- ) VALUES (
---              'sample@example.com',        -- login_id
---              '$2a$10$FjPDD9Y58GVTeOei3JvgVu84kZFxxZSs9j39yOLdkWXOlzOaDrhZe',            -- pwd( passWorld! )
---              '홍길동',                    -- name
---              '010-1234-5678',           -- phone_num
---              '1990-01-01',              -- birth
---              'M',                       -- gender
---              '50',                      -- grade
---              '2024-11-18T23:59:59',     -- join_date (현재 시간)
---              'ACTIVE'                   -- status
---          );
+INSERT INTO member (
+    login_id,
+    pwd,
+    name,
+    phone_num,
+    birth,
+    gender,
+    grade,
+    join_date,
+    status
+) VALUES (
+             'sample@example.com',        -- login_id
+             '$2a$10$FjPDD9Y58GVTeOei3JvgVu84kZFxxZSs9j39yOLdkWXOlzOaDrhZe',            -- pwd( passWorld! )
+             '홍길동',                    -- name
+             '010-1234-5678',           -- phone_num
+             '1990-01-01',              -- birth
+             'M',                       -- gender
+             '50',                      -- grade
+             '2024-11-18T23:59:59',     -- join_date (현재 시간)
+             'ACTIVE'                   -- status
+         );
 --
 -- -- 탈퇴사유
 -- INSERT INTO leave_rsn (reason, active_yn) VALUES
