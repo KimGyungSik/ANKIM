2499a21c356df80dbd600c9aab9fb50b96d315e1
KimGyungSik <0711kyungh@naver.com>
2025-02-26 17:29:27 +0900

    [refactor] full-text Search 적용 사항들 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,1 +1,1 @@
-ALTER TABLE product ADD FULLTEXT INDEX idx_ft_product (name, search_keywords, description);
+# ALTER TABLE product ADD FULLTEXT INDEX idx_ft_product (name, search_keywords, description);
