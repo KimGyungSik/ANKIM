185c1cbea1b6a92885f739240bf30c22cde0dcb5
KimGyungSik <0711kyungh@naver.com>
2025-03-07 22:56:28 +0900

    [feature] 구간별 조회순 상품 조회를 위한 ViewRolling엔티티 설계

---
+++ src/main/java/shoppingmall/ankim/domain/viewRolling/repository/ViewRollingRepository.java
@@ -0,0 +1,7 @@
+package shoppingmall.ankim.domain.viewRolling.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import shoppingmall.ankim.domain.viewRolling.entity.ViewRolling;
+
+public interface ViewRollingRepository extends JpaRepository<ViewRolling,Long> {
+}
---
+++ src/main/java/shoppingmall/ankim/domain/viewRolling/service/ViewRollingSerivce.java
@@ -0,0 +1,11 @@
+package shoppingmall.ankim.domain.viewRolling.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ViewRollingSerivce {
+}
