8f35e5a31f39c9b84b5205fb4a38d8c873a9771b
KimGyungSik <0711kyungh@naver.com>
2025-03-24 20:34:16 +0900

    [test] @Scheduled(fixedRate = ?) + @Async + @SchedulerLock 학습테스트

--- src/test/java/shoppingmall/ankim/global/config/TestSchedulersConfig.java
+++ src/test/java/shoppingmall/ankim/global/config/TestSchedulersConfig.java
@@ -2,13 +2,19 @@ package shoppingmall.ankim.global.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import shoppingmall.ankim.leaning.subject.SchedulerA;
-import shoppingmall.ankim.leaning.subject.SchedulerB;
-import shoppingmall.ankim.leaning.subject.TestAsyncScheduler;
-import shoppingmall.ankim.leaning.subject.TestConflictScheduler;
+import shoppingmall.ankim.leaning.subject.*;
 
 @TestConfiguration
 public class TestSchedulersConfig {
+
+    @Bean
+    public AsyncPollingScheduler asyncPollingScheduler() {
+        return new AsyncPollingScheduler();
+    }
+    @Bean
+    public SyncPollingScheduler syncPollingScheduler() {
+        return new SyncPollingScheduler();
+    }
     @Bean
     public SchedulerA schedulerA() {
         return new SchedulerA();
