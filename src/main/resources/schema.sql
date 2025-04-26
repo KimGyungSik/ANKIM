235e272b58c8e83fba64b0c272f8060fa958dce1
KimGyungSik <0711kyungh@naver.com>
2025-04-26 19:57:46 +0900

    [cicd] schema.sql update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,6 @@
--- 인덱스가 존재하면 삭제
-DROP INDEX idx_ft_product ON product;
-
--- 그리고 새로 생성
 ALTER TABLE product ADD FULLTEXT INDEX idx_ft_product (name, search_keywords, description)
   WITH PARSER ngram;
+
 
 
 CREATE TABLE IF NOT EXISTS shedlock (
