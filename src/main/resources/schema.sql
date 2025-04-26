dd3a6443a22a498ed97ac946ba9e1ced307cb093
KimGyungSik <0711kyungh@naver.com>
2025-04-26 19:15:39 +0900

    [cicd] schema.sql update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,14 @@
+-- 인덱스가 존재하면 삭제
+DROP INDEX idx_ft_product ON product;
+
+-- 그리고 새로 생성
 ALTER TABLE product ADD FULLTEXT INDEX idx_ft_product (name, search_keywords, description)
-   WITH PARSER ngram;
+  WITH PARSER ngram;
 
-CREATE TABLE shedlock (
-                          name VARCHAR(64) NOT NULL PRIMARY KEY,
-                          lock_until TIMESTAMP NOT NULL,
-                          locked_at TIMESTAMP NOT NULL,
-                          locked_by VARCHAR(255) NOT NULL
-);
 
+CREATE TABLE IF NOT EXISTS shedlock (
+                                        name VARCHAR(64) NOT NULL PRIMARY KEY,
+                                        lock_until TIMESTAMP NOT NULL,
+                                        locked_at TIMESTAMP NOT NULL,
+                                        locked_by VARCHAR(255) NOT NULL
+    );
